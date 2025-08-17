bd8598214c72aacab245aed8cabb19c34c0ec0b0
kangjia <ikangjia.cn@outlook.com>
2025-08-17 18:15:30 +0800

    docs: 删除过时的 Markdown指南和测试文件
    - 删除了多个旧的 Markdown 文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://v4.ikangjia.cn',
+
   // output: 'static',
   // integrations: [],
 
@@ -17,6 +20,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       themes: {
@@ -25,4 +29,6 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: vercel(),
 })
